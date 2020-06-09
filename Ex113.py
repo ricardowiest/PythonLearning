f77130c18de885dcc5aaafbb21125efe785f4616
Ricardo Wiest <ricardowiest@gmail.com>
2020-06-09 20:43:14 -0300

    def

---
+++ Ex113.py
@@ -0,0 +1,6 @@
+from uteis.utilidade import dado
+
+ni = dado.leiaInt('Digite um número inteiro: ')
+nr = dado.leiaReal('Digite um número real: ')
+print()
+print(f'\033[1:32:40mO número inteiro digitado foi {ni} e o número real digitado foi {nr} .\033[m')
---
+++ py fin.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader.data as web
+
+import yfinance as yf
+yf.pdr_override()
+
+import seaborn as sns
+
